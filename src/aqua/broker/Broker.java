3404ddd65215127df5afd9eaa69e90d4b6c0120f
Dominik Bartsch <dominik.bartsch@gmx.net>
2024-10-14 18:42:45 +0200

    Finish exercise 2.2

--- src/aqua/broker/Broker.java
+++ src/aqua/broker/Broker.java
@@ -9,6 +9,7 @@ import aqua.common.msgtypes.RegisterResponse;
 import messaging.Endpoint;
 import messaging.Message;
 
+import javax.swing.JOptionPane;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
@@ -25,15 +26,35 @@ public class Broker {
     private final ClientCollection<InetSocketAddress> clientCollection = new ClientCollection<>();
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     private final ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+    private volatile boolean stopRequested = false;
 
     public Broker() {}
 
+    private void requestStop() {
+        stopRequested = true;
+    }
+
     public void broker() {
-        while (true) {
+        // A thread for stopping the server via GUI.
+        // Since the broker loop blocks for receiving a request, this stop is
+        // only executed if a message is received after the user pressed OK.
+        Thread guiThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(null, "Press OK button to terminate server.");
+                requestStop();
+            }
+        });
+        guiThread.start();
+
+        while (!stopRequested) {
             Message message = endpoint.blockingReceive();
             BrokerTask task = new BrokerTask(message);
             executor.execute(task);
         }
+
+        guiThread.interrupt();
+        executor.shutdown();
     }
 
     public static void main(String[] args) {
