6c83d611bc47c66cc9ad9fbaec1eb0126d211d06
Dominik Bartsch <dominik.bartsch@gmx.net>
2024-10-14 18:00:52 +0200

    Finish exercise 2.1

--- src/aqua/broker/Broker.java
+++ src/aqua/broker/Broker.java
@@ -5,22 +5,80 @@ import aqua.common.msgtypes.DeregisterRequest;
 import aqua.common.msgtypes.RegisterRequest;
 import aqua.common.msgtypes.HandoffRequest;
 import aqua.common.msgtypes.RegisterResponse;
+
 import messaging.Endpoint;
 import messaging.Message;
 
 import java.io.Serializable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Broker {
 
-    private final Endpoint endpoint = new Endpoint(4711);
+    private static final String CLIENT_BASE_ID = "tank";
+    private static final int ENDPOINT_PORT = 4711;
+    private static final int NUM_THREADS = 5;
+
+    private final Endpoint endpoint = new Endpoint(ENDPOINT_PORT);
     private final ClientCollection<InetSocketAddress> clientCollection = new ClientCollection<>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
 
     public Broker() {}
 
     public void broker() {
         while (true) {
             Message message = endpoint.blockingReceive();
+            BrokerTask task = new BrokerTask(message);
+            executor.execute(task);
+        }
+    }
+
+    public static void main(String[] args) {
+        Broker broker = new Broker();
+        broker.broker();
+    }
+
+    private class BrokerTask implements Runnable {
+        private final Message message;
+
+        BrokerTask(Message message) {
+            this.message = message;
+        }
+
+        private void register(Message message) {
+            lock.writeLock().lock();
+            String id = CLIENT_BASE_ID + (clientCollection.size() + 1);
+            clientCollection.add(id, message.getSender());
+            lock.writeLock().unlock();
+
+            endpoint.send(message.getSender(), new RegisterResponse(id));
+        }
+
+        private void deregister(Message message) {
+            lock.writeLock().lock();
+            clientCollection.remove(clientCollection.indexOf(message.getSender()));
+            lock.writeLock().unlock();
+        }
+
+        private void handoffFish(Message message) {
+            HandoffRequest payload = (HandoffRequest) message.getPayload();
+            Direction direction = payload.getFish().getDirection();
+
+            lock.readLock().lock();
+            InetSocketAddress neighbor = switch (direction) {
+                case LEFT -> clientCollection.getLeftNeighorOf(clientCollection.indexOf(message.getSender()));
+                case RIGHT -> clientCollection.getRightNeighorOf(clientCollection.indexOf(message.getSender()));
+            };
+            lock.readLock().unlock();
+
+            endpoint.send(neighbor, payload);
+        }
+
+        @Override
+        public void run() {
             Serializable payload = message.getPayload();
 
             if (payload instanceof RegisterRequest) {
@@ -35,34 +93,5 @@ public class Broker {
                 handoffFish(message);
             }
         }
-    }
-
-    private void register(Message message) {
-        String id = "tank" + (clientCollection.size() + 1);
-
-        clientCollection.add(id, message.getSender());
-        System.out.println("registering " + id + " to " + message.getSender());
-        endpoint.send(message.getSender(), new RegisterResponse(id));
-    }
-
-    private void deregister(Message message) {
-        clientCollection.remove(clientCollection.indexOf(message.getSender()));
-    }
-
-    private void handoffFish(Message message) {
-        HandoffRequest payload = (HandoffRequest) message.getPayload();
-        Direction direction = payload.getFish().getDirection();
-
-        InetSocketAddress neighbor = switch (direction) {
-            case LEFT -> clientCollection.getLeftNeighorOf(clientCollection.indexOf(message.getSender()));
-            case RIGHT -> clientCollection.getRightNeighorOf(clientCollection.indexOf(message.getSender()));
-        };
-
-        endpoint.send(neighbor, payload);
-    }
-
-    public static void main(String[] args) {
-        Broker broker = new Broker();
-        broker.broker();
     }
 }
